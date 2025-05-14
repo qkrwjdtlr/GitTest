df2bf9dd5e44b45ae3d933ca16a9d4c9a553767d
qkrwjdtlr <qkrwjdtlrz@naver.com>
2025-05-14 10:13:07 +0900

    start_1 수정사항

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("ok!");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
